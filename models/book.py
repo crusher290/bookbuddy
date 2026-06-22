374c0d6834811e5b2f8ce42bc5798d1332ef507b
crusher <jaderiali025@gmail.com>
2026-06-22 13:46:52 +0330

    date added to the Book class and the atribute current_page change to a privete variable

--- models/book.py
+++ models/book.py
@@ -1,4 +1,5 @@
 from readable import Readable
+from datetime import datetime
 
 class Book(Readable):
     def __init__(self, title:str, author:str, genre:str, total_pages:int):
@@ -6,25 +7,26 @@ class Book(Readable):
         self.author = author
         self.genre = genre
         self.total_pages = total_pages
-        self.current_page = 0
+        self.date_added = datetime.now().strftime("%y/%m/%d,%H:%M:%S")
+        self.__current_page = 0
 
-    @property
+    
     def get_info(self) -> str:
         return f"{self.title} by {self.author} - [genre : {self.genre}] - ({self.total_pages} pages)"
     
     def get_proggres(self) -> float:
-        if self.current_page == 0:
+        if self.__current_page == 0:
             return 0.0
-        proggres = (self.current_page / self.total_pages) * 100
+        proggres = (self.__current_page / self.total_pages) * 100
         return f"{proggres:,.2f}"
         
     
     def update_proggres(self, page_read:int):
         if page_read < 0 :
-            raise ValueError
-        self.current_page += page_read
-        if self.current_page > self.total_pages:
-            self.current_page = self.total_pages
+            raise ValueError("You Can't enter negitive number.")
+        self.__current_page += page_read
+        if self.__current_page > self.total_pages:
+            self.__current_page = self.total_pages
 
 
 
@@ -32,8 +34,9 @@ class Book(Readable):
 
 b = Book("habits","ali","self-help",200)  
 
-print(b.get_info)
+print(b.get_info())
 
-b.update_proggres(100)
+b.update_proggres(101)
 
 print(b.get_proggres())
+
